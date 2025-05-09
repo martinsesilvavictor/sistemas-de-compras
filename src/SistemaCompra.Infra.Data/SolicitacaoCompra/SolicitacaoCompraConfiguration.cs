9554210be50d2a2f3d11afaeeaed325eaf115d69
Victor Silva <martinsesilvavictor@gmail.com>
2025-05-09 20:14:13 -0300

    feature: implementing interface

---
+++ src/SistemaCompra.Infra.Data/SolicitacaoCompra/SolicitacaoCompraConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SolicitacaoCompraAgg = SistemaCompra.Domain.SolicitacaoCompraAggregate;
+
+namespace SistemaCompra.Infra.Data.SolicitacaoCompra
+{
+    public class SolicitacaoCompraConfiguration : IEntityTypeConfiguration<SolicitacaoCompraAgg.SolicitacaoCompra>
+    {
+        public void Configure(EntityTypeBuilder<SolicitacaoCompraAgg.SolicitacaoCompra> builder)
+        {
+            builder.ToTable("SolicitacaoCompra");
+            builder.OwnsOne(u => u.UsuarioSolicitante, n => n.Property("Nome").HasColumnName("UsuarioSolicitante"));
+            builder.OwnsOne(n => n.NomeFornecedor, n => n.Property("Nome").HasColumnName("NomeFornecedor"));
+            builder.OwnsOne(t => t.TotalGeral, n => n.Property("Value").HasColumnName("TotalGeral"));
+            builder.OwnsOne(n => n.CondicaoPagamento, n => n.Property("Valor").HasColumnName("CondicaoPagamento"));
+        }
+    }
+}
---
+++ src/SistemaCompra.Infra.Data/SolicitacaoCompra/SolicitacaoCompraRepository.cs
@@ -0,0 +1,18 @@
+﻿using SolicitacaoCompraAgg = SistemaCompra.Domain.SolicitacaoCompraAggregate;
+
+namespace SistemaCompra.Infra.Data.SolicitacaoCompra
+{
+    public class SolicitacaoCompraRepository : SolicitacaoCompraAgg.ISolicitacaoCompraRepository
+    {
+        private readonly SistemaCompraContext _context;
+        public SolicitacaoCompraRepository(SistemaCompraContext context)
+        {
+            _context = context;
+        }
+
+        public void RegistrarCompra(SolicitacaoCompraAgg.SolicitacaoCompra entity)
+        {
+            _context.Set<SolicitacaoCompraAgg.SolicitacaoCompra>().Add(entity);
+        }
+    }
+}
