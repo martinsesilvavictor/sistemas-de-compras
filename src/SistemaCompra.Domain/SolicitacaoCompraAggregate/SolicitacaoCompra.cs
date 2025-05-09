cf70f8893b2c2aebfd56919b6284df217009b4c9
Victor Silva <martinsesilvavictor@gmail.com>
2025-05-09 19:38:08 -0300

    feature: add validations

--- src/SistemaCompra.Domain/SolicitacaoCompraAggregate/SolicitacaoCompra.cs
+++ src/SistemaCompra.Domain/SolicitacaoCompraAggregate/SolicitacaoCompra.cs
@@ -1,7 +1,6 @@
 ﻿using SistemaCompra.Domain.Core;
 using SistemaCompra.Domain.Core.Model;
 using SistemaCompra.Domain.ProdutoAggregate;
-using SistemaCompra.Domain.SolicitacaoCompraAggregate.Events;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,10 +11,11 @@ namespace SistemaCompra.Domain.SolicitacaoCompraAggregate
     {
         public UsuarioSolicitante UsuarioSolicitante { get; private set; }
         public NomeFornecedor NomeFornecedor { get; private set; }
-        public IList<Item> Itens { get; private set; }
+        public List<Item> Itens { get; private set; }
         public DateTime Data { get; private set; }
         public Money TotalGeral { get; private set; }
         public Situacao Situacao { get; private set; }
+        public CondicaoPagamento CondicaoPagamento { get; set; }
 
         private SolicitacaoCompra() { }
 
@@ -26,6 +26,8 @@ namespace SistemaCompra.Domain.SolicitacaoCompraAggregate
             NomeFornecedor = new NomeFornecedor(nomeFornecedor);
             Data = DateTime.Now;
             Situacao = Situacao.Solicitado;
+
+            Itens = new List<Item>();
         }
 
         public void AdicionarItem(Produto produto, int qtde)
@@ -35,7 +37,21 @@ namespace SistemaCompra.Domain.SolicitacaoCompraAggregate
 
         public void RegistrarCompra(IEnumerable<Item> itens)
         {
-           
+            Itens.AddRange(itens);
+
+            TotalGeral = new Money(Itens.Sum(s => s.Subtotal.Value));
+            TotalGeralPagamento();
+
+            if (!Valido())
+                throw new BusinessRuleException("A soicitação de compra deve conter pelo menos um item.");
         }
+
+        public void TotalGeralPagamento()
+        {
+            if (TotalGeral.Value > 50000)
+                CondicaoPagamento = new CondicaoPagamento(30);
+        }
+
+        public bool Valido() => Itens.Count > 0;
     }
 }
