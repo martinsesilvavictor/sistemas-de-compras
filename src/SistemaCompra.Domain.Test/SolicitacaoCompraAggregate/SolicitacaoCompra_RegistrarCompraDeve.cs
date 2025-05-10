f6d6f39eab21cd848ba8d78630e87d9e586dfb15
Victor Silva <martinsesilvavictor@gmail.com>
2025-05-10 19:12:01 -0300

    test: updating test

--- src/SistemaCompra.Domain.Test/SolicitacaoCompraAggregate/SolicitacaoCompra_RegistrarCompraDeve.cs
+++ src/SistemaCompra.Domain.Test/SolicitacaoCompraAggregate/SolicitacaoCompra_RegistrarCompraDeve.cs
@@ -14,14 +14,14 @@ namespace SistemaCompra.Domain.Test.SolicitacaoCompraAggregate
             //Dado
             var solicitacao = new SolicitacaoCompra("rodrigoasth", "rodrigoasth");
             var itens = new List<Item>();
-            var produto = new Produto("Cedro", "Transversal 3/3", Categoria.Madeira.ToString(), 1001);
-            itens.Add(new Item(produto, 50));
+            var produto = new Produto("Cedro", "Transversal 3/3", Categoria.Madeira.ToString(), 500);
+            itens.Add(new Item(produto, 200));
 
             //Quando
             solicitacao.RegistrarCompra(itens);
 
             //Então
-            //Assert.Equal(30, solicitacao.CondicaoPagamento.Valor);
+            Assert.Equal(30, solicitacao.CondicaoPagamento.Valor);
         }
 
         [Fact]
